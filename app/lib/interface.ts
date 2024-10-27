93e7cb4e2c33fc766838b9b94f705df83bb67e2c
abdellahbouzekri <abdo.bouzekri123@gmail.com>
2024-10-27 17:43:17 +0100

    added vercel ignore and try to fix errors2

--- app/lib/interface.ts
+++ app/lib/interface.ts
@@ -7,9 +7,16 @@ export interface simpleBlogCard {
   currentSlug: string;
   titleImage: any;
 }
+export interface SanityImageAsset {
+  _type: "image";
+  asset: {
+    _ref: string; // Reference to the asset in Sanity
+    _type: "reference";
+  };
+}
 export interface fullBlog {
   currentSlug: string;
   title: string;
   content: TypedObject[];
-  titleImage: { url: string };
+  titleImage: SanityImageAsset;
 }
