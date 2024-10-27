6c102610da0555a91426e31071f0e2ceba74da0a
abdellahbouzekri <abdo.bouzekri123@gmail.com>
2024-10-27 17:57:36 +0100

    added vercel ignore and try to fix errors2

--- app/lib/interface.ts
+++ app/lib/interface.ts
@@ -1,16 +1,16 @@
 import { TypedObject } from "@portabletext/types";
+import { SanityImageObject } from "@sanity/image-url/lib/types/types";
+
 export interface simpleBlogCard {
-  // ...
-  // Add properties here
   title: string;
   smallDescription: string;
   currentSlug: string;
-  titleImage: any;
+  titleImage: SanityImageObject;
 }
 
 export interface fullBlog {
   currentSlug: string;
   title: string;
   content: TypedObject[];
-  titleImage: any;
+  titleImage: SanityImageObject;
 }
